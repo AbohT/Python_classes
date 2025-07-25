eafa2b28b0cd696e72f90122e03ea8aff83a8ad7
ThankGod Audu <thankgodaboh@gmail.com>
2025-07-25 13:33:33 +0100

    Initial commit

---
+++ week7/states.py
@@ -0,0 +1,17 @@
+# Use country names as varibales
+country1 = "Aus"
+country2 = "Malawi"
+country3 = "Russsia"
+country4 = "Nigeria"
+country5 = "Usa"
+country6 = "Iran"
+
+#  list is  a coolection of items in a particualar order
+# Syntax; VARIABLE = [], LIST  constructor; VARIABLE = list()
+# Learn indexing in python
+countries = ["Australia", "Malwai", "Nigeria" "Russia", "Nigeria", "USA", "Iran"]
+#print(countries[0])
+#print(countries[::-1])
+
+
+
